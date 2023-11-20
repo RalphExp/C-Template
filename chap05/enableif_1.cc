4524a6d1bd19c088fed31702de83615fa95ebe79
ralph <ralphxwz@gmail.com>
2023-11-20 23:27:06 +0800

    update

---
+++ chap05/enableif_1.cc
@@ -0,0 +1,37 @@
+#include <utility>
+#include <string>
+#include <iostream>
+#include <type_traits>
+
+using namespace std;
+
+class Person {
+private:
+	std::string name;
+
+public:
+
+    // generic constructor for passed initial name:
+    template<typename S, typename=enable_if_t<!is_same_v<decay_t<S>, Person>, S>>
+    explicit Person(S &&n) : name(std::forward<S>(n)) {
+        std::cout << "TMPL-CONSTR for '" << name << "'\n";
+    }
+
+    // copy and move constructor:
+    Person (Person const& p) : name(p.name) {
+        std::cout << "COPY-CONSTR Person '" << name << "'\n";
+    }
+
+    Person (Person&& p) : name(std::move(p.name)) {
+        std::cout << "MOVE-CONSTR Person '" << name << "'\n";
+    }
+};
+
+int main()
+{
+    std::string s = "sname";
+    Person p1(s); // init with string object => calls copying string-CONSTR
+    Person p2("tmp"); // init with string literal => calls moving string-CONSTR
+    Person p3(p1); // copy Person => calls COPY-CONSTR
+    Person p4(std::move(p1)); // move Person => calls MOVE-CONST
+}
