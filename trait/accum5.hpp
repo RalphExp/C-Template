eb24ae5d2e0caef77e93c7ddb15b4e8272a3e312
ralph <ralphxwz@gmail.com>
2024-01-13 12:55:04 +0900

    update

---
+++ trait/accum5.hpp
@@ -0,0 +1,15 @@
+#ifndef ACCUM5_HPP
+#define ACCUM5_HPP
+
+#include "accumtraits4.hpp"
+template<typename T, typename AT = AccumulationTraits<T>>
+auto accum (T const* beg, T const* end) {
+    typename AT::AccT total = AT::zero();
+    while (beg != end) {
+        total += *beg;
+        ++beg;
+    }
+    return total;
+}
+
+#endif // ACCUM_HPP
