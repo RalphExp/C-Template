e3f61b8f1c67e8c15e4c57ff639e2f1b4be9592b
ralph <ralphexp@example.com>
2024-10-09 10:59:58 -0400

    update

--- traits/hassizetype.hpp
+++ traits/hassizetype.hpp
@@ -8,11 +8,11 @@
 using namespace std;
 
 template <typename T, typename = void_t<>>
-struct HasSizeTypeT : true_type {
+struct HasSizeTypeT : false_type {
 };
 
 template <typename T>
-struct HasSizeTypeT<T, void_t<typename RemoveReference<T>::size_type>> : false_type {
+struct HasSizeTypeT<T, void_t<typename RemoveReference<T>::size_type>> : true_type {
 };
 
 #endif
